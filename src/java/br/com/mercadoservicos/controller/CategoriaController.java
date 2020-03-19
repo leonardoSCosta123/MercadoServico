400c0dc07c63b01b46c4253cc9cfeb65e37e8990
Leonardo Costa <contatositeconsulotoria@gmail.com.br>
2020-03-18 21:58:14 -0300

    Tela de criação, e salvando o bean

--- src/java/br/com/mercadoservicos/controller/CategoriaController.java
+++ src/java/br/com/mercadoservicos/controller/CategoriaController.java
@@ -11,12 +11,11 @@ import javax.faces.bean.RequestScoped;
 
 // transformar managedBean  //fazer a comunicacao com as variaveis com o beckend e fazer a navegacao
 @ManagedBean(name="categoriaMB")
-//definir o escopo
 @RequestScoped
 public class CategoriaController {
     
-    private Integer id;
-    private String descricao;
+//    private Integer id;
+    private Categoria categoria = new Categoria();
     private List<Categoria> categorias;
     private CategoriaService categoriaService = new CategoriaService();
     
@@ -24,38 +23,48 @@ public class CategoriaController {
         listar();//view vai ter referencia na MB,quando o compilador vai le vai instanciar uma categoriaController e vai passar para um construtor e a tabela vai vim prenchida.
     }
     public void listar(){
-        categorias =categoriaService.Listar();//vai ser prenchida pelo service ele pede o dao e o dao buca no banco e manda para o controller.
+        categorias = categoriaService.Listar();//vai ser prenchida pelo service ele pede o dao e o dao buca no banco e manda para o controller.
     }
     //como faz navegaçao de jsp --vai cair na prova, BOTAO DE ACAO CADASTRAR
     public String novo(){
-        return "private/cadastros/categoria/new.xhtml?faces-redirect=true";
-    }//Botao de acao CANCELAR
+        return "new.xhtml?faces-redirect=true";
+    }
+    
+    public String salva(){
+        categoriaService.inserir(categoria);
+        return "list.xhtml?faces-redirect=true";
+    }
+    
+    public String voltar(){
+        return "list.xhtml?faces-redirect=true";
+    }
+    
     public String cancelar(){
-        return "private/cadastros/categoria/list.xhtml?faces-redirect=true";
+        return "list.xhtml?faces-redirect=true";
     }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
+    
     public List<Categoria> getCategorias() {
         return categorias;
     }
 
     public void setCategorias(List<Categoria> categorias) {
         this.categorias = categorias;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public CategoriaService getCategoriaService() {
+        return categoriaService;
+    }
+
+    public void setCategoriaService(CategoriaService categoriaService) {
+        this.categoriaService = categoriaService;
     }
     
     
